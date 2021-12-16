4c825d2d1dd82afd96a1bdec51e174f88dcd9737
Misterius-code <quakemisterius@gmail.com>
2021-12-16 23:16:25 +0000

    a

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ AFRAME.registerComponent('updatelocation', {
 AFRAME.registerComponent('updatelocation', {
     init: function() {
     if(navigator.geolocation) {
-        navigator.geolocation.watchPosition (
+        navigator.geolocation.getPosition (
     
             gpspos=> {
             //    if(ROTO=true){
